cbbeef2708520e50d66328ec3ff5851369495a74
BlackShaman <blackshaman2016@gmail.com>
2024-07-17 19:18:54 +0400

    fonts

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -15,12 +15,23 @@ export default {
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Bona+Nova+SC:ital,wght@0,400;0,700;1,400&display=swap');
 @import "../node_modules/bootstrap/dist/css/bootstrap-grid.min.css";
-
+@font-face {
+  font-family: "ofont"; 
+  src: url("../fonts/ofont.ru_AC_Line.ttf") format("truetype"); 
+  font-style: normal; 
+  font-weight: normal; 
+}
+@font-face {
+  font-family: "eurofurencelight"; 
+  src: url("../fonts/eurofurencelight.ttf") format("truetype"); 
+  font-style: normal; 
+  font-weight: normal; 
+}
 body {
   margin: 0;
 }
 #app {
-  font-family: "Bona Nova SC", serif;
+  font-family: ofont, serif;
   font-weight: 400;
   font-style: normal;
   -webkit-font-smoothing: antialiased;
